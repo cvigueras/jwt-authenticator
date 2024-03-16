38f90c8660728ab126815ef26352478f28f15416
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-03-16 18:01:09 +0100

    test: red GetTokenSuccessfullyAsync

---
+++ Jwt.Authenticator.Api.Test/JwtauthFeature.cs
@@ -0,0 +1,37 @@
+using Jwt.Authenticator.Api.Test.Fixtures;
+using Jwt.Authenticator.Api.Test.Startup;
+using Newtonsoft.Json;
+using NUnit.Framework.Internal;
+
+namespace Jwt.Authenticator.Api.Test
+{
+    public class JwtauthFeature
+    {
+        public const string RequestUriBase = "Auth";
+        public const string PathJson = "./Fixtures/user.json";
+        private JwtAuthenticatorClient client;
+
+        [SetUp]
+        public void Setup()
+        {
+            client = new JwtAuthenticatorClient(new SetupFixture().CreateClient());
+        }
+
+        [Test]
+        public async Task GetTokenSuccessfullyAsync()
+        {
+            var json = await client.GetJsonContent(PathJson);
+
+            var responsePost = await client.Post(RequestUriBase, json);
+
+            var token = responsePost.Content.ReadAsStringAsync().Result;
+
+            var tokenResult = JsonConvert.DeserializeObject<TokenDto>(token);
+       
+
+            var settings = new VerifySettings();
+
+            await Verify(tokenResult, settings);
+        }
+    }
+}
---
+++ Jwt.Authenticator.Api/TokenDto.cs
@@ -0,0 +1,5 @@
+﻿namespace Jwt.Authenticator.Api.Test
+{
+    public record TokenDto(string access_token, string refresh_token, string expires_in);
+    
+}

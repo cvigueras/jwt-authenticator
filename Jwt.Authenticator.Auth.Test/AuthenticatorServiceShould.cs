4590a68e419e9178af5855bcce89749f7e178165
Carlos Vigueras <carlosj.vigueras@gmail.com>
2024-03-16 16:53:46 +0100

    refactor: extract method test to mock configuration builder.

--- Jwt.Authenticator.Auth.Test/AuthenticatorServiceShould.cs
+++ Jwt.Authenticator.Auth.Test/AuthenticatorServiceShould.cs
@@ -10,7 +10,6 @@ namespace Jwt.Authenticator.Auth.Test
     {
         private const int ExpirationInSeconds = 3600;
         private AuthenticatorService authenticatorService;
-        private IConfiguration configuration;
         private IEnumerable<Claim> claims;
         string user = "user";
         string email = "juan@juanito.com";
@@ -28,7 +27,8 @@ namespace Jwt.Authenticator.Auth.Test
         [Test]
         public void GetTokenUserSuccesfully()
         {
-            MockConfigurationBuilder("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            var configuration = MockConfigurationBuilder.SetConfg("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            authenticatorService = new AuthenticatorService(configuration);
 
             var token = authenticatorService.GenerateAccessToken(claims);
 
@@ -38,7 +38,8 @@ namespace Jwt.Authenticator.Auth.Test
         [Test]
         public void AuthSuccessfullyByToken()
         {
-            MockConfigurationBuilder("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            var configuration = MockConfigurationBuilder.SetConfg("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            authenticatorService = new AuthenticatorService(configuration);
             var token = authenticatorService.GenerateAccessToken(claims);
 
             var result = authenticatorService.ValidateJwtToken(token.access_token);
@@ -59,7 +60,8 @@ namespace Jwt.Authenticator.Auth.Test
         [Test]
         public void GetArgumentOutOfRangeExceptionWhenSecretKeyIsTooShort()
         {
-            MockConfigurationBuilder("SecretKey", "Test.com", ExpirationInSeconds);
+            var configuration = MockConfigurationBuilder.SetConfg("SecretKey", "Test.com", ExpirationInSeconds);
+            authenticatorService = new AuthenticatorService(configuration);
 
             Action result = () => authenticatorService.GenerateAccessToken(claims);
 
@@ -70,7 +72,8 @@ namespace Jwt.Authenticator.Auth.Test
         [Test]
         public void ExpiresTokenInOneHour()
         {
-            MockConfigurationBuilder("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            var configuration = MockConfigurationBuilder.SetConfg("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            authenticatorService = new AuthenticatorService(configuration);
 
             var token = authenticatorService.GenerateAccessToken(claims);
 
@@ -80,25 +83,12 @@ namespace Jwt.Authenticator.Auth.Test
         [Test]
         public void GetRefreshTokenUserSuccesfully()
         {
-            MockConfigurationBuilder("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            var configuration = MockConfigurationBuilder.SetConfg("SecretKey_1111111111100000000011", "Test.com", ExpirationInSeconds);
+            authenticatorService = new AuthenticatorService(configuration);
 
             var refresh_token = authenticatorService.GenerateRefreshToken();
 
             refresh_token.Should().NotBeNull();
-        }
-
-        private void MockConfigurationBuilder(string key, string issuer, int expiration)
-        {
-            var inMemorySettings = new Dictionary<string, string> {
-                {"Jwt:Issuer", issuer },
-                {"Jwt:Key", key },
-                {"Jwt:Expiration", expiration.ToString() },
-                {"Jwt:Audicence", "https://localhost:5001" },
-            };
-            configuration = new ConfigurationBuilder()
-                            .AddInMemoryCollection(inMemorySettings)
-                            .Build();
-            authenticatorService = new AuthenticatorService(configuration);
         }
     }
 }
---
+++ Jwt.Authenticator.Auth.Test/MockConfigurationBuilder.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Jwt.Authenticator.Auth.Test
+{
+    public static class MockConfigurationBuilder
+    {
+        public static IConfiguration SetConfg(string key, string issuer, int expiration)
+        {
+            var inMemorySettings = new Dictionary<string, string> {
+                {"Jwt:Issuer", issuer },
+                {"Jwt:Key", key },
+                {"Jwt:Expiration", expiration.ToString() },
+                {"Jwt:Audicence", "https://localhost:5001" },
+            };
+            return new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemorySettings)
+                            .Build();
+        }
+    }
+}
